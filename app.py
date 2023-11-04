0ef178ecd391ba6357e3a0a30c2ac7c48a080be1
manikala950 <140065089+manikala950@users.noreply.github.com>
2023-11-04 11:54:25 +0530

    TASK3

--- app.py
+++ app.py
@@ -1,86 +1,53 @@
-# Import the necessary libraries
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from flask import Flask, request, jsonify
 import pickle
 
-from flask import Flask, render_template, request
-import pymongo
+# Load the Iris dataset
+data = pd.read_csv('C:\\Users\\manik\\Desktop\\New folder (2)\\iris.csv')
+
+# Split the data into features (X) and the target variable (y)
+X = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+y = data['species']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and train a random forest classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Save the trained model to a file using pickle
+with open('iris_model.pkl', 'wb') as model_file:
+    pickle.dump(model, model_file)
+
+# Load the trained model
+with open('iris_model.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
 
 app = Flask(__name__)
 
-# Load your dataset (replace 'data.csv' with your dataset file)
-data = pd.read_csv('C:\\Users\\manik\\Desktop\\New folder (3)\\house.csv')
-
-# Feature selection
-corrmat = data.corr()
-top_corr_features = corrmat.index
-threshold = 0.1
-selected_features = corrmat.columns[corrmat.abs().mean() > threshold]
-print("Selected features are:", selected_features)
-
-# Split the data into training and testing sets
-X = data[['Avg. Area Income','Avg. Area House Age','Avg. Area Number of Rooms','Avg. Area Number of Bedrooms','Area Population']]
-y = data['Price']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create and train the linear regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
-
-# Make predictions
-y_pred = model.predict(X_test)
-
-# Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-print("Mean Squared Error:", mse)
-print("R-squared:", r2)
-
-# Save the trained model to a file
-with open('model.pkl', 'wb') as model_file:
-    pickle.dump(model, model_file)
-
-# Set up the MongoDB connection
-client = pymongo.MongoClient('mongodb://localhost:27017/')
-db = client['HOME']  # Replace 'your_database_name' with your actual database name
-collection = db["PREDICTION"]
-# Define a route for rendering the HTML form
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Iris Flower Species Prediction"
 
-# Define a route for handling the form submission and storing data in MongoDB
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extract input data from the form
-    income = float(request.form['income'])
-    house_age = float(request.form['house_age'])
-    num_rooms = float(request.form['num_rooms'])
-    num_bedrooms = float(request.form['num_bedrooms'])
-    population = float(request.form['population'])
+    try:
+        data = request.get_json()
+        sepal_length = data['sepal_length']
+        sepal_width = data['sepal_width']
+        petal_length = data['petal_length']
+        petal_width = data['petal_width']
 
-    # Perform your prediction using the machine learning model
-    input_data = [[income, house_age, num_rooms, num_bedrooms, population]]
-    predicted_price = model.predict(input_data)[0]
+        # Make a prediction using the loaded model
+        prediction = loaded_model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
 
-    # Save the prediction data to MongoDB
-    prediction_data = {
-        'income': income,
-        'house_age': house_age,
-        'num_rooms': num_rooms,
-        'num_bedrooms': num_bedrooms,
-        'population': population,
-        'predicted_price': predicted_price
-    }
+        return jsonify({'prediction': prediction})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-    
-    collection.insert_one(prediction_data)
-
-    # Render the result page with the prediction result
-    return render_template('result.html', predicted_price=predicted_price)
-
-if __name__ == '__main__':
+if __name__ == '__main':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,52 +1,39 @@
-# Import the necessary libraries
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 import pickle
 
-# Load your dataset (replace 'data.csv' with your dataset file)
-data = pd.read_csv('C:\\Users\\manik\\Desktop\\New folder (3)\\house.csv')
+# Load the Iris dataset
+data = pd.read_csv('C:\\Users\\manik\\Desktop\\New folder (2)\\iris.csv')
 
-# Feature selection
-corrmat = data.corr()
-top_corr_features = corrmat.index
-plt.figure(figsize=(11, 11))
-g = sns.heatmap(data[top_corr_features].corr(), annot=True, cmap="RdYlGn")
-plt.show()
-threshold = 0.4
-selected_features = corrmat.columns[corrmat.abs().mean() > threshold]
-print("Selected features are:", selected_features)
+# Split the data into features (X) and the target variable (y)
+X = data[['sepal_length','sepal_width','petal_length','petal_width']]
+y = data['species']
 
 # Split the data into training and testing sets
-X = data[selected_features]
-y = data['Price']
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create and train the linear regression model
-model = LinearRegression()
+# Create and train a random forest classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 # Make predictions
 y_pred = model.predict(X_test)
 
 # Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
 
-print("Mean Squared Error:", mse)
-print("R-squared:", r2)
+print("Accuracy:", accuracy)
+print("Classification Report:\n", report)
 
-# Save the trained model to a file
-with open('model.pkl', 'wb') as model_file:
+# Save the trained model to a file using pickle
+with open('iris_model.pkl', 'wb') as model_file:
     pickle.dump(model, model_file)
 
 # Later, you can load the model and use it for predictions
-with open('model.pkl', 'rb') as model_file:
+with open('iris_model.pkl', 'rb') as model_file:
     loaded_model = pickle.load(model_file)
 
 
